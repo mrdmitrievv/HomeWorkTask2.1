cdfd8f66818702d630693bd35e93fece92f0f591
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-02-23 12:08:05 +0300

    Initial Commit

---
+++ HomeWorkTask2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkTask2.1
+//
+//  Created by Артём Дмитриев on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
